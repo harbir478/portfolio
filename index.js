91554dfa589b9a6bebdff8a1543cbb9a9649592f
Harbir4 <harbir478@gmail.com>
2018-06-17 18:54:06 -0700

    Buttons Functionality

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 AOS.init();
 
+$(".btn--btnDiv").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".heading-tertiary").offset().top},
+        'slow');
+});
+
+
+$(".projectBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".projects").offset().top},
+        'slow');
+});
+
