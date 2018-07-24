4e98bf320b8ede283f3ee67475c1818e5a9ccbfe
Harbir4 <harbir478@gmail.com>
2018-07-23 22:32:18 -0700

    navbar added

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 
+  
+    $('.navbar__items__social').click(function() {
+      $('.sicon').toggle("slide"); 
+    });
+
 AOS.init({
  })
+
+
+ $(".navbar__logo").click(function() {
+    $('html,body').animate({
+        scrollTop: $('.header').offset().top});
+});
+
+ $(".navbar__items__about").click(function() {
+    $('html,body').animate({
+        scrollTop: $('.heading-tertiary').offset().top},
+        'slow');
+});
+
+$(".navbar__items__projects").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".projects").offset().top},
+        'slow');
+});
+
+$(".navbar__items__contact").click(function() {
+    $('html,body').animate({
+        scrollTop: $('.footer').offset().top},
+        'slow');
+});
 
 $(".btn--btnDiv").click(function() {
     $('html,body').animate({
