c313ebfd40c98a818e878bd1d856ed4a420cbe70
Harbir4 <harbir478@gmail.com>
2018-08-13 22:50:20 -0700

    added browser support

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
-
-  
-    $('.navbar__items__social').click(function() {
-      $('.sicon').toggle("slide"); 
-    });
-
-AOS.init({
- })
-
-
- $(".navbar__logo").click(function() {
-    $('html,body').animate({
-        scrollTop: $('.header').offset().top});
+$(".navbar__items__social").click(function() {
+  $(".sicon").toggle("slide");
 });
 
- $(".navbar__items__about").click(function() {
-    $('html,body').animate({
-        scrollTop: $('.heading-tertiary').offset().top},
-        'slow');
+AOS.init({});
+
+$(".navbar__logo").click(function() {
+  $("html,body").animate({
+    scrollTop: $(".header").offset().top
+  });
+});
+
+$(".navbar__items__about").click(function() {
+  $("html,body").animate(
+    {
+      scrollTop: $(".header__none").offset().top
+    },
+    "slow"
+  );
 });
 
 $(".navbar__items__projects").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".projects").offset().top},
-        'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".projects").offset().top
+    },
+    "slow"
+  );
 });
 
 $(".navbar__items__contact").click(function() {
-    $('html,body').animate({
-        scrollTop: $('.footer').offset().top},
-        'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".footer").offset().top
+    },
+    "slow"
+  );
 });
 
 $(".btn--btnDiv").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".heading-tertiary").offset().top},
-        'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".header__none").offset().top
+    },
+    "slow"
+  );
 });
-
 
 $(".projectBtn").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".projects").offset().top},
-        'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".projects").offset().top
+    },
+    "slow"
+  );
 });
-
